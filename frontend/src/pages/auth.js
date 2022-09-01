6e2ea0e5cb6600033890772c9b7e15c30d233aff
Billbern <denis.abaidoo@gmail.com>
2022-09-01 17:29:50 +0000

    created various components and pages

---
+++ frontend/src/pages/auth.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Authentication extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Login</p>
+                <input type="button" value="login" />
+            </div>
+        );
+    }
+}
---
+++ frontend/src/pages/client.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default class Client extends React.Component{
+    render(){
+
+        return(
+            <div>
+                <p>Client Page</p>
+            </div>
+        );
+    }
+}
